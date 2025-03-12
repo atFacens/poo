00ad8df0908f36d4ab3eb6f82d99d8206a740198
Emerson Paduan <emerson@paduan.dev.br>
2025-03-12 20:47:39 -0300

    aula 04

--- aula04/AppRelogio.java
+++ aula04/AppRelogio.java
@@ -2,15 +2,15 @@ public class AppRelogio {
     public static void main(String[] args) {
         
         Relogio relogio1 = new Relogio();
-        Relogio relogio2 = new Relogio();
+        Relogio relogio2 = new Relogio(80);
 
         relogio1.exibirHora();
-        // relogio1.hora = 20; // somente public
-        relogio1.exibirHora();
-
-        relogio2.alterarHora(9);
         relogio2.exibirHora();
         
+        relogio2.setHora(9);
+        System.out.println("Hora = " + relogio2.getHora());
+
+        relogio2.exibirHora();
 
     }
 }
--- aula04/Relogio.java
+++ aula04/Relogio.java
@@ -2,14 +2,35 @@ public class Relogio {
     // atributos
     private int hora, minuto, segundo;
 
+    // construtor
+    // métodos especial chamado na construção do objeto
+
+    public Relogio() {
+
+    }
+
+    // sobrecarga: overload
+    public Relogio(int novaHora) {
+        setHora(novaHora);
+    }
+
+    public Relogio(int novaHora, int minuto) {
+        setHora(novaHora);
+    }
+
     // métodos
     public void exibirHora() {
         System.out.println(hora + ":" + minuto + ":" + segundo);
     }
 
-    public void alterarHora(int novaHora) {
-        if(novaHora >= 0 && novaHora < 24) {
-            hora = novaHora;
+    public void setHora(int hora) {
+        if (hora >= 0 && hora < 24) {
+            // this é uma referência ao próprio obj
+            this.hora = hora;
         }
+    }
+
+    public int getHora() {
+        return hora;
     }
 }
